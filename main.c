304c1cbfd6b7e279d92b13867852544ee01ec2cc
ehab mostafa <ehab02166@gmail.com>
2021-07-10 12:17:04 +0200

    structs

--- main.c
+++ main.c
@@ -1,9 +1,34 @@
+//                                      LINKED LISTS PROJECT
+// Team members:-
+//Name1 : Thabet hussien thabet          sec:1                BN: 49
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-int main()
+typedef struct listnode{
+    char Student_Name[30];
+    int Student_ID;
+    int Score_Of_Last_Year;
+    int Day_Of_Birth;
+    int Year_Of_Birth;
+    int Month_Of_Birth;
+    struct listnode* next;
+}ListNode;
+
+typedef struct dynamicarray
 {
+    char Student_Name[30];
+    int Student_ID;
+    int Score_Of_Last_Year;
+    int Day_Of_Birth;
+    int Year_Of_Birth;
+    int Month_Of_Birth;
+}DynamicArray;
 
-
-
-}
+typedef struct list
+{
+    ListNode* head;
+    ListNode* tail;
+    size_t size;
+}List;
