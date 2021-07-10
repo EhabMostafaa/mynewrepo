edae36b2d727d4d023c4c98ac1a34ce72138a425
ehab mostafa <ehab02166@gmail.com>
2021-07-10 12:32:43 +0200

    prototypes

--- main.c
+++ main.c
@@ -32,3 +32,19 @@ typedef struct list
     ListNode* tail;
     size_t size;
 }List;
+void initializeList(List *);
+void designList(int, List *);
+void printList(List *);
+ListNode* enterInformationOfStudent();// enter information of a student in linked list part
+void enterInformationOfStudent2(DynamicArray*);//enter information of a student in dynamic array part
+void insertInTheBeginning(List *);//linked list
+void insertInTheEnd(List *);//linked list
+void insert_In_Nth_Position_In_The_Middle(List *);//linked list
+void print_dynamic_array_elements(DynamicArray*);
+void Insert_in_the_beginning_of_dynamic_array(DynamicArray*);
+void Insert_in_the_middle_of_dynamic_array(DynamicArray*);
+void Insert_in_the_end_of_dynamic_array(DynamicArray*);
+
+int n;
+
+
